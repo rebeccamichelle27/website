9a76e4b0d0d67d55cdc98bf4a3af3af09c646f5d
Rebecca Smith <beccamichelle@live.com>
2021-03-27 16:58:10 -0500

    update profile

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Orbit - Bootstrap 4 Resume/CV Template for Developers</title>
+        <title>Rebecca Smith</title>
         {/* Meta */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Responsive HTML5 Resume/CV Template for Developers" />
-        <meta name="author" content="Xiaoying Riley at 3rd Wave Media" />
         <link rel="shortcut icon" href="favicon.ico" />
         {/* Google Font */}
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" />
